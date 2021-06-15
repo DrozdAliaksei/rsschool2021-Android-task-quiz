8799efeb171211b2aa0a3da2986874dd4de18048
Drozd Aliaksei <aliakseidrozd@yandex.ru>
2021-06-15 16:50:17 +0300

    fead: started filling in the template

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.rsschool.quiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.rsschool.quiz.databinding.ActivityMainBinding
+import com.rsschool.quiz.databinding.FragmentQuizBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        with(binding) {
+            setContentView(root)
+
+            viewPager.adapter = QuizAdapter()
+            viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+            viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+
+                }
+            })
+        }
     }
+
+    inner class QuizAdapter : RecyclerView.Adapter<QuizViewHolder>() {
+        override fun onBindViewHolder(holder: QuizViewHolder, position: Int) {
+            TODO("Not yet implemented")
+            holder.binding.question.text = "questions[position]"
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = QuizViewHolder(
+            FragmentQuizBinding.inflate(layoutInflater, parent, false)
+        )
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+            //return quizQuestions.size
+        }
+    }
+
+    inner class QuizViewHolder(val binding: FragmentQuizBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
