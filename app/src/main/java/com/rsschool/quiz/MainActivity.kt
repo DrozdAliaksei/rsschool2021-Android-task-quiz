d6b073a7c68831244411384625e1de9c27e65222
Drozd Aliaksei <aliakseidrozd@yandex.ru>
2021-06-16 17:05:28 +0300

    fead: off scrolling, add listeners

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rsschool.quiz
 import android.os.Bundle
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.rsschool.quiz.databinding.ActivityMainBinding
@@ -11,6 +12,7 @@ import com.rsschool.quiz.databinding.FragmentQuizBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var userAnswers: Array<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,26 +20,42 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
             setContentView(root)
-            viewPager.adapter = QuizAdapter()
+            viewPager.adapter = QuizAdapter(viewPager)
             viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
             viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-
+                override fun onPageScrollStateChanged(state: Int) {
+                    super.onPageScrollStateChanged(state)
+                    viewPager.isUserInputEnabled = false
                 }
             })
         }
     }
 
-    inner class QuizAdapter : RecyclerView.Adapter<QuizViewHolder>() {
+    inner class QuizAdapter(val viewPager2: ViewPager2) : RecyclerView.Adapter<QuizViewHolder>() {
         override fun onBindViewHolder(holder: QuizViewHolder, position: Int) {
             when (position) {
-                0 -> setData(holder, QuizQuestions.One)
+                0 -> {
+                    setData(holder, QuizQuestions.One)
+                    holder.binding.apply {
+                        previousButton.isVisible = false
+                        toolbar.navigationIcon = null
+                    }
+                }
                 1 -> setData(holder, QuizQuestions.Two)
                 2 -> setData(holder, QuizQuestions.Three)
                 3 -> setData(holder, QuizQuestions.Four)
-                4 -> setData(holder, QuizQuestions.Five)
+                4 -> {
+                    setData(holder, QuizQuestions.Five)
+                    holder.binding.nextButton.text = "Submit"
+                }
+            }
+            holder.binding.previousButton.setOnClickListener {
+                viewPager2.currentItem = position - 1
+            }
+            holder.binding.nextButton.setOnClickListener {
+                if (position != 4) viewPager2.currentItem = position + 1
+                //else open new activity with result
             }
 
             holder.binding.radioGroup.setOnCheckedChangeListener { group, i ->
